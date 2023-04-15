30ff83a3de2f667c024f9fa446bfef90676ac9db
Abhishek Kartalkar <abhishekk001dev@gmail.com>
2023-04-15 11:36:09 +0530

    added css file and admin dash

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,9 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/admin', 'Home::admin');
+$routes->get('/dashboard', 'Home::dashboard');
 
 /*
  * --------------------------------------------------------------------
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,16 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+
+
+    public function admin()
+    {
+        return view('./admin/dist/admin_login_page');
+    }
+
+    public function dashboard()
+    {
+        return view('./admin/dist/dashboard');
+    }
 }
